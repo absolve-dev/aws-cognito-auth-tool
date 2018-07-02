27564cad78785a79bfc2bfb40634abdc57ac478d
ccsalazar <ccsalazar200@gmail.com>
2018-07-02 14:50:08 -0700

    created react control form for email and password signup

--- src/containers/Authentication/Signup.js
+++ src/containers/Authentication/Signup.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
+import 'bulma/css/bulma.css'
+
 
 class Signup extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Signup</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          email: "",
+          password: "",
+          confirmPassword: "",
+
+        };
+    }
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div class="field">
+                    <div class="control">
+                        <input 
+                            type="text" 
+                            class="input is-primary" 
+                            placeholder="Email"
+                            name="email" 
+                            value={this.state.email}
+                            onChange={this.handleChange} 
+                        />
+                    </div>
+                    <div class="control">
+                        <input 
+                            type="text" 
+                            class="input is-primary" 
+                            placeholder="Password"
+                            name="password" 
+                            value={this.state.password} 
+                            onChange={this.handleChange} 
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Signup;
