c0ba00e26cb1338054f261b453dba07857e9ec19
ccsalazar <ccsalazar200@gmail.com>
2018-07-02 18:04:13 -0700

    add login route to app router

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Login from "./containers/Authentication/Login";
 import Signup from "./containers/Authentication/Signup";
 import CognitoSetup from "./containers/Authentication/CognitoSetup";
 import EndpointForm from "./containers/EndpointForm";
@@ -11,7 +12,7 @@ class AppRouter extends Component {
         return(
             <Router>
                 <Switch>
-                    <Route exact path="/" component={Signup}/>
+                    <Route exact path="/" component={Login}/>
                     <Route path="/signup" component={Signup}/>
                     <Route path="/cognito-setup" component={CognitoSetup}/>
                     <Route path="/endpoints" component={EndpointForm}/>
