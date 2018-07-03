d45c6eb27df89148364f3dbd176289c5e58a54ab
ccsalazar <ccsalazar200@gmail.com>
2018-07-02 18:03:48 -0700

    add login form

---
+++ src/containers/Authentication/Login.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Auth } from "aws-amplify";
+import { Redirect } from 'react-router-dom'
+import 'bulma/css/bulma.css'
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          email: "",
+          password: "",
+          redirect: false
+        };
+    }
+    handleSubmit = async event => {
+        event.preventDefault()
+        if (this.email === "" &&
+            this.password === ""){
+                return
+            }
+        try {
+            const response = await Auth.signIn(this.state.email.toLowerCase(), this.state.password);
+            console.log(response);
+            this.setState({redirect:true})
+        } catch (error) {
+            console.log(error);  
+        }
+    }
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    render() {
+        if (this.state.redirect){
+            return (
+                <Redirect to="/endpoints"/>
+            )
+        }
+        return (
+            <div className="App">
+                <h1 className="title">User Login Info</h1>
+                <div className="field">
+                    <div className="control">
+                        <input 
+                            type="text" 
+                            className="input is-primary" 
+                            placeholder="Email"
+                            name="email" 
+                            value={this.state.email}
+                            onChange={this.handleChange} 
+                        />
+                    </div>
+                </div>
+                <div className="field">
+                    <div className="control">
+                        <input 
+                            type="text" 
+                            className="input is-primary" 
+                            placeholder="Password"
+                            name="password" 
+                            value={this.state.password} 
+                            onChange={this.handleChange} 
+                        />
+                    </div>
+                </div>
+                <a className="button is-info" onClick={this.handleSubmit}>Submit</a>
+            </div>
+        );
+    }
+}
+
+export default Login;
