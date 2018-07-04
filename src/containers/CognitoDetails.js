69a4ad1033ebf9bb2acc8899e4b515b94326fd6c
ccsalazar <ccsalazar200@gmail.com>
2018-07-03 18:41:45 -0700

    create new component to display cognito settings

---
+++ src/containers/CognitoDetails.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom'
+
+class CognitoDetails extends Component {
+
+    
+    render() {
+        return(
+            <div className="container is-fullhd">
+                <div className="notification flex-space-between" >
+                    <div className="card">
+                    <header className="card-header">
+                        <p className="card-header-title">
+                        Cognito Settings
+                        </p>
+                    </header>
+                    <div className="card-content">
+                        <p className="text-align-left"><strong>region: </strong>{this.props.cognito.region}</p>
+                        <p className="text-align-left"><strong>identityPoolId: </strong>{this.props.cognito.identityPoolId}</p>
+                        <p className="text-align-left"><strong>userPoolId: </strong>{this.props.cognito.userPoolId}</p>
+                        <p className="text-align-left"><strong>userPoolWebClientId: </strong>{this.props.cognito.userPoolWebClientId}</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CognitoDetails;
