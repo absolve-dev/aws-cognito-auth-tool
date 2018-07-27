0fd09c9fd1515961ac0000f21551ccf5143abd67
ccsalazar <ccsalazar200@gmail.com>
2018-07-26 17:57:00 -0700

    remove unnecessary code

--- src/containers/EndpointLists.js
+++ src/containers/EndpointLists.js
@@ -46,7 +46,6 @@ class EndpointLists extends Component {
     render() {
         return (
             <div className="App">
-                <h1 className="title">Endpoints To Test</h1>
                 {
                     this.props.endpoints.length === 0 ? null:
                     this.props.endpoints.map( (endpoint,index) => {
